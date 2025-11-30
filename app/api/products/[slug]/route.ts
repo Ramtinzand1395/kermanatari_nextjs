55b878737a06858f5780c461261aa54d036d7380
ramtin.zand <ramtinzand6@gmail.com>
2025-11-30 11:29:51 +0330

    first commit9

--- app/api/products/[slug]/route.ts
+++ app/api/products/[slug]/route.ts
@@ -1,12 +1,46 @@
-// app/api/products/[slug]/route.ts
-import { NextResponse } from "next/server";
+// // app/api/products/[slug]/route.ts
+// import { NextResponse } from "next/server";
+// import { prisma } from "@/lib/prisma";
+
+// export async function GET(
+//   req: Request,
+//   context: { params: Promise<{ id: string }> }
+// ) {
+//   const { slug } = await context.params;
+//   if (!slug) {
+//     return NextResponse.json({ error: "Slug موجود نیست" }, { status: 400 });
+//   }
+
+//   try {
+//     const product = await prisma.product.findUnique({
+//       where: { slug },
+//       include: {
+//         images: true,
+//         specifications: { include: { items: true } },
+//         category: true,
+//         tags: true,
+//         comments: {
+//           where: { verified: true }, // فقط کامنت‌های تایید شده
+//         },
+//       },
+//     });
+//     if (!product)
+//       return NextResponse.json({ error: "محصول پیدا نشد" }, { status: 404 });
+//     return NextResponse.json(product);
+//   } catch (err) {
+//     console.error(err);
+//     return NextResponse.json({ error: "خطای داخلی" }, { status: 500 });
+//   }
+// }
+
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(
-  req: Request,
-  context: { params: Promise<{ id: string }> }
+  req: NextRequest,
+  context: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = await context.params;
+  const { slug } = await context.params; // ✅ unwrap Promise
   if (!slug) {
     return NextResponse.json({ error: "Slug موجود نیست" }, { status: 400 });
   }
@@ -19,13 +53,14 @@ export async function GET(
         specifications: { include: { items: true } },
         category: true,
         tags: true,
-        comments: {
-          where: { verified: true }, // فقط کامنت‌های تایید شده
-        },
+        comments: { where: { verified: true } },
       },
     });
-    if (!product)
+
+    if (!product) {
       return NextResponse.json({ error: "محصول پیدا نشد" }, { status: 404 });
+    }
+
     return NextResponse.json(product);
   } catch (err) {
     console.error(err);
